ca51db5316b063566f6289a30b946f9d97a7bd1e
satya <kvsnarayana89@gmail.com>
2019-02-16 13:47:42 +0530

    added permutations program

--- src/main/java/com/example/actuator/api/endpoint/ListDemo.java
+++ src/main/java/com/example/actuator/api/endpoint/ListDemo.java
@@ -8,18 +8,16 @@ public class ListDemo {
 
 	public static void main(String[] args) {
 		List<String> l = new ArrayList<>();
-		int n = 1;
 		for (int i = 1; i <= 10; i++) {
 			l.add("satya" + i);
 		}
-		System.out.println(l.get(6));
+		
 		Iterator<String> itr = l.iterator();
-		while (n<=6) {
-			System.out.println("ListDemo.main()");
-				System.out.println(itr.next());
-				n++;
-			}
+		
+		/*for (int n = 1; n <= 6; n++) {
+			System.out.print(itr.next() + "\t");
+		}*/
+		
 
-		}
 	}
-
+}
